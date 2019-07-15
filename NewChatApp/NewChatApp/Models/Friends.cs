de3894a8b81d6c7aa247697c92b079166f416208
Ivo <ivo_radev2@abv.bg>
2019-07-15 18:53:42 +0300

    icon + modelFriends

--- NewChatApp/NewChatApp/Models/Friends.cs
+++ NewChatApp/NewChatApp/Models/Friends.cs
@@ -8,7 +8,25 @@ namespace NewChatApp.Models
     {
         public List<string> Invitations { get; private set; }
         public List<string> FriendsUsernames { get; private set; }
-        public string RemoveFriendUsername { get; private set; }
-        public void CheckInvitationList() { }
+        public void CheckInvitationList()//TODO
+        {
+
+        }
+        public void ShowFriendList()//TODO
+        {
+
+        }
+        public void RemoveFriendFromList(string friendName)
+        {
+            if (this.FriendsUsernames.Contains(friendName))
+            {
+                this.FriendsUsernames.Remove(friendName);
+            }
+            else
+            {
+                throw new InvalidOperationException("You do not have such friend");
+            }
+        }
+        
     }
 }
