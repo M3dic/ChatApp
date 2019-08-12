66877cfec030721819ea2876e2d477a7613b5218
M3dic <ivo_radev2@abv.bg>
2019-08-12 16:17:37 +0300

    changing async operation for connecting to mysql

--- MyChatApplicationAzureServiceBus.net core/MyChatApplicationAzureServiceBus/Constructor/FriendsConstructorBaseInput.cs
+++ MyChatApplicationAzureServiceBus.net core/MyChatApplicationAzureServiceBus/Constructor/FriendsConstructorBaseInput.cs
@@ -7,11 +7,11 @@ namespace MyChatApplicationAzureServiceBus.Constructor
 {
     public class FriendsConstructorBaseInput
     {
-        private MySqlConnection connection;
         private string server;
         private string database;
         private string uid;
         private string password;
+        private string connectionString;
 
         public FriendsConstructorBaseInput()
         {
@@ -24,50 +24,8 @@ namespace MyChatApplicationAzureServiceBus.Constructor
             database = "ChatApp";
             uid = "Ivo";
             password = "123456789";
-            string connectionString;
             connectionString = "SERVER=" + server + ";" + "DATABASE=" +
             database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
-
-            connection = new MySqlConnection(connectionString);
-        }
-
-        //open connection to database
-        private bool OpenConnection()
-        {
-            try
-            {
-                connection.OpenAsync();
-                return true;
-            }
-            catch (MySqlException ex)
-            {
-                switch (ex.Number)
-                {
-                    case 0:
-                        Console.WriteLine("Cannot connect to server.  Contact administrator");
-                        break;
-
-                    case 1045:
-                        Console.WriteLine("Invalid username/password, please try again");
-                        break;
-                }
-                return false;
-            }
-        }
-
-        //Close connection
-        private bool CloseConnection()
-        {
-            try
-            {
-                connection.CloseAsync();
-                return true;
-            }
-            catch (MySqlException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
         }
 
         internal void InviteFriend(string usernaem, string friendname)
@@ -75,8 +33,9 @@ namespace MyChatApplicationAzureServiceBus.Constructor
             string query = $"INSERT INTO Friends" +
                 $" (UserName,FriendsUsername) " +
                 $"VALUES('{usernaem}', '{friendname}')";
-            using (connection.OpenAsync())
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
+                connection.OpenAsync();
                 //create command and assign the query and connection from the constructor
                 MySqlCommand cmd = new MySqlCommand(query, connection);
 
